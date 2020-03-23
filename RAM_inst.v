3ecc6ab085cb26769883da1ea93710700819da83
JayAwkWyn <j.armatage12@gmail.com>
2020-03-23 11:19:20 -0700

    Created a new branch containing a processor datapath with paging. The perl script within this branch is used to generate .sv files like id and ustore.

--- RAM_inst.v
+++
@@ -1,9 +0,0 @@
-RAM	RAM_inst (
-	.clken ( clken_sig ),
-	.clock ( clock_sig ),
-	.init ( init_sig ),
-	.dataout ( dataout_sig ),
-	.init_busy ( init_busy_sig ),
-	.ram_address ( ram_address_sig ),
-	.ram_wren ( ram_wren_sig )
-	);
